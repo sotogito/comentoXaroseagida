4e0ff60b2e1fda2371af8dd7a268e8c765599dea
sotogito <108535526+sotogito@users.noreply.github.com>
2023-07-28 16:20:28 +0900

    입력받기 전

--- mymail/models.py
+++ mymail/models.py
@@ -8,8 +8,14 @@ class PrevLetter(models.Model):
     level_type = models.IntegerField(default=0) #previousQuestions[id-1][1]
     question = models.IntegerField(default=0) #previousQuestions[id-1][2]
     option = models.IntegerField(default=0) #previousQuestions[id-1][3]
-
     question_text = models.CharField(max_length=200) #문자열, 이전 편지를 띄움
     is_active = models.BooleanField(default=True) #사용자가 편지 그림을 그렸는지 안그렸는지 True or False
 
+class UserAnswer(models.Model):
+    question = models.ForeignKey(PrevLetter, on_delete=models.CASCADE)
+    user_answer_text = models.CharField(max_length=200)
+    # 다른 필드들을 추가할 수 있음 (사용자의 이름, 날짜 등)
+
+    def __str__(self):
+        return f"{self.question.question_text}: {self.user_answer_text}"
 
--- mymail/views.py
+++ mymail/views.py
@@ -5,7 +5,10 @@ from .models import PrevLetter
 def mymail(request):
     active_count = PrevLetter.objects.filter(is_active=True).count()
     question_list = PrevLetter.objects.order_by('-id').filter(is_active=True)
-    question_context = {'question_list' : question_list , 'active_count': active_count}
+    question_context = {
+        'question_list' : question_list ,
+        'active_count': active_count
+    }
     return render(request, 'mymail/mymail.html',question_context)
 
 def blockmail(request):
